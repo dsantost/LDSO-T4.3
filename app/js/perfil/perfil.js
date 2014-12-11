9c143454cc263b301d3b8b3edaabdc837f1b95df
João Monteiro <joaomiguelmonteiro@gmail.com>
2014-12-11 07:24:42 +0000

    Profile finished :+1:
    Incorporação de angularjs à página estática concluída.

--- app/js/perfil/perfil.js
+++ app/js/perfil/perfil.js
@@ -1,47 +1,184 @@
 'use strict'
 var profile= angular.module('profile',[]);
 
+
 profile.filter('unsafe', function($sce) {
-    return function(val) {
-        return $sce.trustAsHtml(val);
-    };
+	return function(val) {
+		return $sce.trustAsHtml(val);
+	};
 });
 
-profile.controller('BackgroundCtrl', function() {
-  this.student = studentVar;
-  this.interests = interestsVar;
+profile.controller('bgCtrl', function() {
+	this.student=studentVar;
+});
+
+profile.controller('volCtrl',function(){
+	this.vol=volunteeringVar;
+});
+
+profile.controller('skillsCtrl',function(){
+	this.skills=skillsVar;
+});
+
+profile.controller('certsCtrl',function(){
+	this.certificates=certsVar;
+});
+
+profile.controller('langsCtrl',function(){
+	this.languages=langsVar;
+});
+
+profile.controller('eduCtrl',function(){
+	this.education=eduVar;
+});
+
+profile.controller('intsCtrl',function(){
+	this.interests=intsVar;
+});
+
+profile.controller('portCtrl',function(){
+	this.portfolio=portVar;
 });
 
 var studentVar = {
-        name: 'João Monteiro',
-        local: 'Porto, Portugal',
-        bgImg:'url(img/banner.jpg)',
-        birthdate: '9 de Maio de 1993',
-        civilstatus: 'Solteiro',
-        institution:'Faculdade de Engenharia da Universidade do Porto',
-        institutionabb:'FEUP',
-        course:'Mestrado Integrado em Engenharia Informática e Computação',
-        courseabb:'MIEIC'
+	name: 'João Monteiro',
+	profilepic: 'img/team/4.jpg',
+	local: 'Porto, Portugal',
+	bgImg:'url(img/banner.jpg)',
+	birthdate: '9 de Maio de 1993',
+	civilstatus: 'Solteiro',
+	institution:'Faculdade de Engenharia da Universidade do Porto',
+	institutionabb:'FEUP',
+	course:'Mestrado Integrado em Engenharia Informática e Computação',
+	courseabb:'MIEIC'
 };
-/*
-var interestsVar = [
-	{
-		Bem-estar animal
-	},
-	{
-		Arte e Cultura
-	},
-	{
-		Ação social
-	},
-	{
-		Educação
-	},
-	{
-		Ambiente
-	},
-	{
-		Direitos humanos
-	},
-]
-	*/
+
+var volunteeringVar = {
+	interests:[
+		{
+			name: 'Bem-estar animal'
+		},
+		{
+			name: 'Arte e Cultura'
+		},
+		{
+			name: 'Ação social'
+		},
+		{
+			name: 'Educação'
+		},
+		{
+			name: 'Ambiente'
+		},
+		{
+			name: 'Direitos humanos'
+		},
+	],
+	done:[
+		{
+			name: 'Banco Alimentar'
+		},
+	]
+};
+
+var skillsVar = {
+	best:[
+		{
+			name: 'C++'
+		},
+		{
+			name: 'Java'
+		},
+		{
+			name: 'HTML'
+		},
+	],
+	moderate:[
+		{
+			name: 'Desenvolvimento ágil de software'
+		},
+		{
+			name: 'AngularJS'
+		},
+	]
+};
+
+var certsVar = {
+	list:[
+		{
+			name: 'Certificate of Proficiency in English',
+			grade: 'A',
+			entity: 'Universidade de Cambridge',
+			vality: 'Ilimitada'
+		},
+	]
+};
+
+var langsVar = {
+	fluent:[
+		{
+			name: 'Português'
+		},
+		{
+			name: 'Inglês'
+		},
+	],
+	basic:[
+		{
+			name: 'Francês'
+		},
+		{
+			name: 'Espanhol'
+		},
+	]
+};
+
+var eduVar = {
+	secondary:[
+		{
+			name: 'Escola Secundária Dr. Mário Sacramento',
+			year: '12.º ano'
+		},
+	],
+	superior:[
+		{
+			name: 'Faculdade de Engenharia da Universidade do Porto',
+			degree: 'Mestrado Integrado em Engenharia Informática e Computação'
+		},
+	]
+};
+
+
+var intsVar = {
+	list:[
+		{
+			name: 'Fotografia'
+		},
+		{
+			name: 'Literatura'
+		},
+		{
+			name: 'Cinematografia'
+		},
+	]
+};
+
+var portVar = {
+	items: [
+		{
+			name: 'Jogo "Raiden"',
+			degree: 'MIEIC',
+			img: 'img/UP/mieic_p1.jpg'
+		},
+		{
+			name: 'uni-X"',
+			degree: 'MIEIC',
+			img: 'img/UP/mieic_p2.jpg'
+		},
+		{
+			name: 'Apresentação "Rosewood Hotels"',
+			degree: 'MIEIC',
+			img: 'img/UP/mieic_p3.jpg'
+		},
+	]
+}
