ae87f0ee9b7752778e36a5df183e01fe9c1ea525
diogo92 <diogo_p_ribeiro@hotmail.com>
2014-12-26 02:41:48 +0000

    started authentication

--- app/js/app.js
+++ app/js/app.js
@@ -36,3 +36,45 @@ uniX.controller('registerController',function($scope,$http){
     }
      
 });
+
+uniX.controller('loginController',function($scope,$http,$window){
+    $scope.user = {username:'',password:''};
+    $scope.message = '';
+    $scope.login = function () { 
+    $http
+    .post('/authenticate/',$scope.user)
+    .success(function (data,status,headers,config){
+      $window.sessionStorage.toke=data.token;
+        $scope.message = 'Seja bem vindo';
+    })
+    .error(function (data,status,headers,config){
+        delete $window.sessionStorage.token;
+        $scope.message = 'Error: Invalid user of password';
+    });
+    }
+     
+});
+
+uniX.factory('authInterceptor', function ($rootScope, $q, $window) {
+  return {
+    request: function (config) {
+      config.headers = config.headers || {};
+      if ($window.sessionStorage.token) {
+        config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
+      }
+      return config;
+    },
+    response: function (response) {
+      if (response.status === 401) {
+          
+      }
+      return response || $q.when(response);
+    }
+  };
+});
+
+uniX.config(function ($httpProvider) {
+  $httpProvider.interceptors.push('authInterceptor');
+});
+
+
